f7c3978bd045a3a588740fd51eebb32f15c7ee5c
ShruthiMeenakshi <shruthikumar542@gmail.com>
2026-03-19 17:50:38 +0530

    resolved cors issues

--- backend/src/main/java/com/netflixclone/api/controller/MovieController.java
+++ backend/src/main/java/com/netflixclone/api/controller/MovieController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://localhost:5173",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://cine-flix-topaz.vercel.app"
 })
 public class MovieController {
 
